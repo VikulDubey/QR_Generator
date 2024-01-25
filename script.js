47e7f8aa72efab49b7d0df49eb587b924690944a
Vikul Dubey <vikuldubey43@gmail.com>
2024-01-25 15:43:00 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const userInput = document.getElementById("userInput");
+const generateBtn = document.querySelector(".generateBtn");
+const qrImage = document.querySelector(".qrImage");
+const errorMsg = document.querySelector(".errorMsg");
+
+async function generateQR(inputValue) {
+  try {
+    if (!inputValue) {
+      throw new Error("Please enter a valid input");
+    } else {
+      const response = await fetch(
+        `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=encodeURI(${inputValue})`
+      );
+      const qrURL = response.url;
+      console.log(qrURL);
+      qrImage.src = qrURL;
+    }
+  } catch (e) {
+    errorMsg.innerHTML = e.message;
+    errorMsg.setAttribute("class", "showError");
+  }
+}
+
+generateBtn.addEventListener("click", () => {
+  const inputValue = userInput.value;
+  generateQR(inputValue);
+  userInput.value = "";
+});
